b3350cb8e27efe63f7bbf7b06bdc999469b4a487
james55555 <grahamj1@aston.ac.uk>
2014-03-01 23:27:33 +0000

    Changed password

--- FinalYearProject/Includes/common/Database.class.php
+++ FinalYearProject/Includes/common/Database.class.php
@@ -4,13 +4,13 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
+
 /**
  * Description of Database_Instance
  *
  * @author grahamj1
  */
-class Database
-    {
+class Database {
 
     //Database Connection
     protected $conn;
@@ -20,7 +20,7 @@ class Database
     private
             $DB_user = 'root';
     private
-            $DB_pwd = 'lug9bull';
+            $DB_pwd = '';
     private
             $DB_nm = 'FYP';
 
@@ -29,74 +29,60 @@ class Database
      */
 
     public //static
-            function connect()
-        {
+            function connect() {
 
         $this->conn = mysql_connect($this->DB_server, $this->DB_user, $this->DB_pwd, true) or die("Unable to Connect to MySQL\n" . mysqli_connect_error());
-        if (is_resource($this->conn))
-            {
+        if (is_resource($this->conn)) {
             mysql_select_db($this->DB_nm, $this->conn) or die("Unable to Select Database \n" . mysqli_connect_error());
-            }
         }
+    }
 
     /*
      * Function to close database connection
      */
 
     public
-            function close()
-        {
-        if (mysql_ping() !== null)
-            {
-            if (mysql_close($this->conn))
-                {
+            function close() {
+        if (mysql_ping() !== null) {
+            if (mysql_close($this->conn)) {
                 $this->conn = null;
-                }
             }
-        else
-            {
+        } else {
             throw new Exception("Error closing database!");
-            }
         }
+    }
 
     /*
      * Singleton to query database and return object
      */
 
     public
-            function selectQuery($query, $model)
-        {
+            function selectQuery($query, $model) {
         //Run the query against the database and assign the result set to result variable
         $result = mysql_query($query, $this->conn);
         //Ensure query success has returned correctly
-        if (Database::querySuccess($result))
-            {
-            if (mysql_num_rows($result) > 0)
-                {
+        if (Database::querySuccess($result)) {
+            if (mysql_num_rows($result) > 0) {
                 //Fetch result and assign it to row variable
                 $row = mysql_fetch_object($result);
                 //Create a new instance of the object using result set
                 $instance = new $model($row);
-                }
-            else
-                {
+            } else {
                 $instance = null;
                 //**TESTING: waht happens if instance is null... All models should handle this
                 echo "<br/>instance is null. Rows returned is: " . mysql_num_rows($result);
-                }
             }
-        else
-            {
+        } else {
             //Die and print issue with query
             die("Issue running query: " . mysql_error()
                     . "<br/>The query is: " . $query
                     . "<br/>Died...");
-            }
+        }
         //Close database and return the instance
         Database::close();
 
         return $instance;
-        }
+    }
 
     /*
      * Function to ascertain success of query
@@ -104,23 +90,20 @@ class Database
      */
 
     public
-            function querySuccess($result)
-        {
+            function querySuccess($result) {
         //if the result of the query is true (i.e. has returned without fail)
         //then assign @boolean success true 
-        if ($result === false)
-            {
+        if ($result === false) {
             $success = false;
-            }
+        }
         //else echo the error and return @boolean success false
-        else
-            {
+        else {
 
             $success = true;
-            }
-        return $success;
         }
-
+        return $success;
     }
+
+}
 
 ?>

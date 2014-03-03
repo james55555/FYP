9a943607c404ecf48cdd65b1781405332921b597
james55555 <grahamj1@aston.ac.uk>
2014-03-03 10:41:12 +0000

    Config file added... require statement added to point to it.

--- FinalYearProject/index.php
+++ FinalYearProject/index.php
@@ -7,32 +7,7 @@
  *  Description     : initiate master class @autload
  * 
  */
-
-
-/*
- * Turn error reporting on and start session
- */
-error_reporting(E_ALL);
-
-//Set up path constant for site
-$site_path = realpath(dirname(__FILE__));
-$sitePath = str_replace('\\', '/', $site_path);
-define('__SITE_PATH', $sitePath);
-
-$site_url = $_SERVER['REQUEST_URI'];
-//Set up path to parent/abstract classes
-define('__COMMON_PATH', __SITE_PATH . '/Includes/common/');
-
-//Set up path to Models
-define('__MODEL_PATH', __SITE_PATH . '/Includes/System/Model/');
-
-//Set up path to Controllers
-define('__CONTROLLER_PATH', __SITE_PATH . '/Includes/System/Controller/');
-
-//Set up path to Views
-define('__VIEW_PATH', __SITE_PATH . '/Includes/System/View/');
-
-include __SITE_PATH .'/vendor/autoload.php';
+require "configuration.php";
 
 //Start session
 session_start();

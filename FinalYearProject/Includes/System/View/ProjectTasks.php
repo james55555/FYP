df1d5c8ac469717dd2ab1bd9f20469aade75f92a
james55555 <grahamj1@aston.ac.uk>
2014-03-09 19:40:43 +0000

    played around printing out N/A if estimation is null

--- FinalYearProject/Includes/System/View/ProjectTasks.php
+++ FinalYearProject/Includes/System/View/ProjectTasks.php
@@ -21,57 +21,59 @@
         <div id="container">
             <h1>Tasks for project: </h1>
             <input type="button" id="addTask" class="button" value="Add"/>
-            <table id="myTasks" class="table">
-                <tr>
-                    <th>Project ID</th>
-                    <th>Task ID</th>
-                    <th>Task Name</th>
-                    <th>Task Description</th>
-                    <th>Status</th>
-                </tr>
-                <tr>
-                    <?php
-                    if ($this->registry->project_tasks !== false)
-                        {
-                        foreach ($this->registry->project_tasks as $task)
-                            {
-                            //Collect estimate for each task
-                            $estimate = Estimation_Model::get($task->estimation());
 
-                            echo "<td>{$task->proj_id()}</td>" .
-                            "<td>{$task->tsk_id()}</td>" .
-                            "<td>{$task->tsk_nm()}</td>" .
-                            "<td>{$task->tsk_dscr()}</td>" .
-                            "<td>{$task->tsk_status()}</td><tr/>";
-                            //temp
-                            }
+
+
+            <?php
+            if ($this->registry->project_tasks !== false)
+                {
+                foreach ($this->registry->project_tasks as $task)
+                    {
+                    ?> 
+
+                    <table id="myTasks" class="table">
+                        <tr>
+                            <th>Project ID</th>
+                            <th>Task ID</th>
+                            <th>Task Name</th>
+                            <th>Task Description</th>
+                            <th>Status</th>
+                        </tr>
+                        <?php
+                        //for every task obtain it's estimate information
+                        $estimate = Estimation_Model::get($task->estimation());
+
+                        echo "<td>{$task->proj_id()}</td>" .
+                        "<td>{$task->tsk_id()}</td>" .
+                        "<td>{$task->tsk_nm()}</td>" .
+                        "<td>{$task->tsk_dscr()}</td>" .
+                        "<td>{$task->tsk_status()}</td><tr/>";
                         ?>           
 
-                </table>
-            <?php var_dump($task); ?>
-                <table id="myTaskTimes" class="table"><tr>
-                        <th>Expected Finish</th>
-                        <th>Actual Finish</th>
-                        <th>Assigned to</th>
-                        <th>Action</th>
-                    </tr>
-                    <?php
-                    echo "<tr>";
-                    if ($estimate !== false)
-                        {
-                        echo "<td>{$estimate->est_end_dt()}</td>" .
-                        "<td>{$estimate->act_end_dt()}</td>";
-                        } else
-                        {
-                        echo "<td>No estimate found!</td>" .
-                        "<td>No estimate found!</td>";
-                        }
-                    "<td>task->staff-></td>" .
+                        <table id="myTaskTimes" class="table"><tr>
+                                <th>Expected Finish</th>
+                                <th>Actual Finish</th>
+                                <th>Assigned to</th>
+                                <th>Action</th>
+                            </tr>
+                            <?php
+                            echo "<tr>";
+                            if ($estimate !== false)
+                                {
+                                echo "<td>{$estimate->est_end_dt()}</td>" .
+                                "<td>{$estimate->act_end_dt()}</td>";
+                                } else
+                                {
+                                echo "<td>No estimate found!</td>" .
+                                "<td>No estimate found!</td>";
+                                }
+                            echo "<td>task->staff-></td>" .
                             "<td>add or delete</td>" .
                             "</tr>";
-                    }
-                ?>
-
+                            }
+                        }
+                    ?>
+                </table>
 
             </table>
             <?php include("footer.php"); ?>

55ba5aaee8c8cd62fe0a6c9ec4096b1d4a192a21
james55555 <grahamj1@aston.ac.uk>
2014-03-09 12:52:15 +0000

    removed commented out code

--- FinalYearProject/Includes/System/Controller/Task_Controller.php
+++ FinalYearProject/Includes/System/Controller/Task_Controller.php
@@ -19,23 +19,7 @@ class Task_Controller extends Main_Controller
         {
 
         $this->registry->project_tasks = Task_Model::getAllTasks($_GET['proj_id']);
-        $this->registry->user_tasks = Task_Model::getallUserTasks
-                ($_SESSION['user']->userId());
-        //   $this->registry->estimations = Estimation_Model::getAllEstimates($_GET['id']);
-        //   $this->registry->staffMembers = Staff_Model::getStaffForProject($_GET['id']);
 
-        /*
-         * Test Statements
-         */
-        /*
-          echo "<br/>***VAR_TEST****<br/>"
-          . "id: " . var_dump($id)
-          . "<br/>key: " . var_dump($key)
-          . "<br/>projTasks: " . var_dump($projTasks) .
-          "<br/>statusDescription: " . var_dump($statusDescription)
-          . "<br/>taskStatus: " . var_dump($taskStatus)
-          . "<br/>registry taskStatus: " . var_dump($this->registry->taskStatus);
-         * */
 
         //Show the projectTasks view
         $this->registry->View_Template->show('projectTasks');

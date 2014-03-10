73b8d0d5f4343eefcb7d0f9b0d4f28e00d65723c
james55555 <grahamj1@aston.ac.uk>
2014-03-10 22:20:46 +0000

    logo image added and sourced.

--- FinalYearProject/Includes/System/View/header.php
+++ FinalYearProject/Includes/System/View/header.php
@@ -8,16 +8,26 @@
  * 
  */
 ?>
-<div id="headerWrapper" class="header">
-    <div id="headerList" class="header">
-        <ul>
-            <li><a href="?page=Home">Home - My Projects</a></li>
-            <li><a href="?page=Reports">Reports</a></li>
-            <li><a href="?page=Calendar">Calendar</a></li>
-            <li><a href="?page=Timeline">Timeline</a></li>
-            <li><a href="?page=Logout">Logout</a></li>
-        </ul>
+    <link rel="stylesheet" type="text/css" href="Includes/CSS/header.css"/>
+    <div id="wrapperHeader">
+        <div id="header">
+            <!--Set up logo header that if clicked on takes user to their home page-->
+            
+            <ul>
+                <a href="?page=Home">
+                    <!--Aston University, UK Logo 
+                    (Copyrighted official logo for Aston University, 2007) -->
+                        <img 
+                            src="http://upload.wikimedia.org/wikipedia/commons/b/b5/Aston_University_Logo.png" 
+                            alt="University Logo"/>
+                        </a>
+                <li><a href="?page=Home">Home - My Projects</a></li>
+                <li><a href="?page=Reports">Reports</a></li>
+                <li><a href="?page=Calendar">Calendar</a></li>
+                <li><a href="?page=Timeline">Timeline</a></li>
+                <li><a href="?page=Logout">Logout</a></li>
+            </ul>
+        </div>
     </div>
-</div>
-<br style="clear:"/>
+
 

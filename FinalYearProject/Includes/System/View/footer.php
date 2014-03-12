db8140331191a906271aca4c38d301a2140817fa
james55555 <grahamj1@aston.ac.uk>
2014-03-12 22:04:46 +0000

    added if statement for registration page

--- FinalYearProject/Includes/System/View/footer.php
+++ FinalYearProject/Includes/System/View/footer.php
@@ -15,13 +15,28 @@
     <div id="left" class="footer">
         <p>Candidate Number: 522511</p>
     </div>
-    
+
     <div id="right" class="footer">
         <p>&copy 2014, James Graham</p>
     </div>
-    
-    <div id="middle" class="footer">
-        <a href="#top">Back to Top</a></br>
-        <p>Individual Project - CS3010</p>
-    </div>
+
+
+    <?php if ($_GET['page'] !== "Register")
+        { ?>
+        <div id="middle" class="footer">
+            <a href="#top">Back to Top</a></br>
+            <p>Individual Project - CS3010</p>
+        </div>
+    <?php
+        } else
+        {
+        ?>
+        <div id="middleRegister" class="footer">
+            <p>Final Year Project</p>
+        </div>
+        <?php
+        }
+    ?>
+
+
 </div>

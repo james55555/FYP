ff6198fc28be61119cffda2981a823674516ed08
james55555 <grahamj1@aston.ac.uk>
2014-03-17 19:29:20 +0000

    removed reference to university project

--- FinalYearProject/Includes/System/View/footer.php
+++ FinalYearProject/Includes/System/View/footer.php
@@ -12,31 +12,10 @@
 <link rel="stylesheet" type="text/css" href="Includes/CSS/footer.css"/> 
 
 <div id="wrapperFooter">
-    <div id="left" class="footer">
-        <p>Candidate Number: 522511</p>
-    </div>
-
     <div id="right" class="footer">
         <p>&copy 2014, James Graham</p>
     </div>
-
-
-    <?php if ($_GET['page'] !== "Register")
-        { ?>
-        <div id="middle" class="footer">
-            <a href="#top">Back to Top</a></br>
-            <p>Individual Project - CS3010</p>
-        </div>
-    <?php
-        } else
-        {
-        ?>
-        <div id="middleRegister" class="footer">
-            <p>Final Year Project</p>
-        </div>
-        <?php
-        }
-    ?>
-
-
+    <div id="middle" class="footer">
+        <a href="#top">Back to Top</a>
+    </div>
 </div>

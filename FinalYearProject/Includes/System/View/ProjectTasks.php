5ec225796611364a08af55609cd325ed1671e1f8
james55555 <grahamj1@aston.ac.uk>
2014-03-20 11:08:17 +0000

    altered tsk_id href to post tsk_id

--- FinalYearProject/Includes/System/View/ProjectTasks.php
+++ FinalYearProject/Includes/System/View/ProjectTasks.php
@@ -78,7 +78,7 @@
                                     $staff = Staff_Model::get($task->staff());
                                     $tsk_id = $task->tsk_id();
                                     echo "<tr><td>"
-                                    . "<a href=\"?page=Task&arg=$tsk_id" . $tsk_id . "\" \">"
+                                    . "<a href=\"?page=task&action=details&task_id=$tsk_id\" \">"
                                     . $tsk_id
                                     . "</a>"
                                     . "</td>" .

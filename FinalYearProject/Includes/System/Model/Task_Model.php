92de760a874222b275dcae9c5f7d1bdd7e7092ef
james55555 <grahamj1@aston.ac.uk>
2014-03-20 11:46:36 +0000

    added if statement to constructor

--- FinalYearProject/Includes/System/Model/Task_Model.php
+++ FinalYearProject/Includes/System/Model/Task_Model.php
@@ -1,16 +1,11 @@
 <?php
 
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
  * Description of Task_Model
  *
  * @author James
  */
+
 class Task_Model
     {
 
@@ -38,15 +33,21 @@ class Task_Model
         {
         $this->tsk_id = $row->TSK_ID;
         //Get the project corresponding to the linked id
-        $_assocProj = Project_Model::getProject($_GET['proj_id']);
-        //Assign the associated
-        $this->proj_id = $_assocProj->proj_id();
-        //$_assocStatus = Status_Model::getStatus($this->TSK_ID);
+        if (isset($_GET['proj_id']))
+            {
+            $_assocProj = Project_Model::getProject($_GET['proj_id']);
+            //Assign the associated
+            $this->proj_id = $_assocProj->proj_id();
+            }
+            else{
+                $this->proj_id = $this->proj_id();
+                }
+
         $this->status = $row->STATUS;
         $this->task_nm = $row->TASK_NM;
         $this->web_addr = $row->WEB_ADDR;
         $this->tsk_dscr = $row->TSK_DESCR;
-        
+
         $this->estimation = TaskEstimation_Model::getEstimationId($row->TSK_ID);
         $this->staff = StaffTask_Model::getStaffId($row->TSK_ID);
         }
@@ -92,9 +93,11 @@ class Task_Model
         {
         return $this->estimation;
         }
-        public function staff(){
-            return $this->staff;
-            }
+
+    public function staff()
+        {
+        return $this->staff;
+        }
 
     /*
      * method to get a task by its id.
@@ -109,7 +112,8 @@ class Task_Model
         $db = new Database();
         $db->connect();
 
-        $query = "SELECT DISTINCT TSK_ID, PROJ_ID, STATUS, TASK_NM, WEB_ADDR, TSK_DESCR"
+        $query = "SELECT DISTINCT TSK_ID, PROJ_ID, "
+                . "STATUS, TASK_NM, WEB_ADDR, TSK_DESCR"
                 . " FROM TASK "
                 . "WHERE tsk_id='$tsk_id'";
 
@@ -146,7 +150,7 @@ class Task_Model
         $db->close();
         return $project_tasks;
         }
-        
+
     /*
      * Function to edit/update a task given the provided parameters
      * @param int tsk_id, int proj_id, int status_id, String tsk_nm, String web_addr, String tsk_descr, obj Model estimation, obj Model dependency

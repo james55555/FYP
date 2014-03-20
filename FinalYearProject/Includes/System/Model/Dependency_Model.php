17b00622d210a19836934d2365b55d9b2b72794f
james55555 <grahamj1@aston.ac.uk>
2014-03-20 14:52:30 +0000

    set up get method and constructor to handle null returns

--- FinalYearProject/Includes/System/Model/Dependency_Model.php
+++ FinalYearProject/Includes/System/Model/Dependency_Model.php
@@ -1,22 +1,41 @@
 <?php
+
 /*
  * Description of Dependency_Model
  *
  * @author James
  */
+
 class Dependency_Model
     {
+
     private $dpnd_id;
     private $dpnd_on;
-    
-    public function __construct($row){
-        $this->dpnd_id = $row->DEPENDENCY_ID;
-        $this->dpnd_on = $row->DEPENDENT_ON;
-    }
-    public function dpnd_id(){
+
+    public function __construct($row)
+        {
+        if (isset($row))
+            {
+            $this->dpnd_id = $row->DEPENDENCY_ID;
+            $this->dpnd_on = $row->DEPENDENT_ON;
+            }
+        }
+
+    public function dpnd_id()
+        {
         return $this->dpnd_id;
         }
-        public function dpnd_on(){
-            return $this->dpnd_on;
-            }
+
+    public function dpnd_on()
+        {
+        return $this->dpnd_on;
+        }
+
+    public static function get($dpnd_id)
+        {
+        $columns = array("DEPENDENCY_ID", "DEPENDENT_ON");
+        $dependency = Database_Queries::getOneCol($columns, "DEPENDENCY", "DEPENDENCY_ID", $dpnd_id);
+        return $dependency;
+        }
+
     }

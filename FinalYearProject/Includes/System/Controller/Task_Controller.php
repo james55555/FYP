65b9d6c0ece31ae6c9d8165614b2726e1308c87d
james55555 <grahamj1@aston.ac.uk>
2014-03-20 14:10:46 +0000

    made task dependencies available to the view

--- FinalYearProject/Includes/System/Controller/Task_Controller.php
+++ FinalYearProject/Includes/System/Controller/Task_Controller.php
@@ -7,47 +7,50 @@
  */
 class Task_Controller extends Main_Controller
     {
-
     /*
      * Function to set task variables based on project data and 
      * display high-level task view
      */
+
     public
             function main()
         {
 
         $this->registry->project_tasks = Task_Model::getAllTasks($_GET['proj_id']);
-        if(sizeof($this->registry->project_tasks) === 0){
-          $this->registry->project_tasks = null;
-        }
+        if (sizeof($this->registry->project_tasks) === 0)
+            {
+            $this->registry->project_tasks = null;
+            }
         $project = Project_Model::getProject($_GET['proj_id']);
         $this->registry->projectEstimation = Estimation_Model::get($project->estimation());
         //Show the projectTasks view
         $this->registry->View_Template->show('projectTasks');
         }
 
-        /*
-         * Function to set individual task variables and 
-         * show the task details page
-         */
+    /*
+     * Function to set individual task variables and 
+     * show the task details page
+     */
+
     public function details()
         {
         $this->registry->task = Task_Model::getTask($_GET['task_id']);
-        $this->registry->taskEstimation = 
-                Estimation_Model::get($this->registry->task->estimation());
-        $this->registry->taskStaff =      
-                Staff_Model::get($this->registry->task->staff());
-         
+        $this->registry->taskEstimation = Estimation_Model::get($this->registry->task->estimation());
+        $this->registry->taskStaff = Staff_Model::get($this->registry->task->staff());
+        $this->registry->taskDependencies = Dependency_Model::get($this->registry->task->dpnd());
+
         $this->registry->View_Template->show('taskDetails');
         }
+
+    public function addTask()
+        {
+
+        $this->registry->View_Template->show('addTask');
+        }
+
+    public function editTask()
+        {
         
-        public function addTask(){
-            
-            $this->registry->View_Template->show('addTask');
-            }
-        public function editTask()
-            {
-            
-            }
+        }
 
     }

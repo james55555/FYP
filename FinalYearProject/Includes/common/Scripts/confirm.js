409acea1f3e0c264c1ddc6a986a3849a34de3ac2
james55555 <grahamj1@aston.ac.uk>
2014-03-21 10:10:54 +0000

    confirm issue resolved (script parsing err)

--- FinalYearProject/Includes/common/Scripts/confirm.js
+++ FinalYearProject/Includes/common/Scripts/confirm.js
@@ -2,5 +2,5 @@
  * Function to prompt the user to confirm their action choice
  */
 function confirm(item){
-    return confirm("Are you sure you want to delete " + item + "?");
+    return confirm("Are you sure you want to delete" + item + "?");
 }

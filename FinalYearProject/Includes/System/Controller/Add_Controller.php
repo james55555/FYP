7bac5e48158ff1ade56c87cf0e15234b018babf2
james55555 <grahamj1@aston.ac.uk>
2014-03-28 02:56:55 +0000

    created methods to add new project

--- FinalYearProject/Includes/System/Controller/Add_Controller.php
+++ FinalYearProject/Includes/System/Controller/Add_Controller.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Description of Add_Controller
  *
@@ -6,14 +7,74 @@
  */
 class Add_Controller extends Main_Controller
     {
-    public function main(){
-        
-        $this->registry->View_Template->show('message');
-        }
-        public function addProject(){
-            
+
+    private $isProject = false;
+    protected $newProject;
+
+    public function main()
+        {
+        //if is task or is project)
+        if ($_GET['page'] === 'Home')
+            {
+            $this->isProject = true;
+            $this->registry->View_Template->show('addProject');
+            } else
+            {
+            $this->registry->View_Template->show('addTask');
             }
-            public function addTask() {
-                
+        }
+
+    /*
+     * @param boolean $err - if is an error
+     * @param String $errDetails - then print out details
+     */
+
+    public function showError($err, $errDetails)
+        {
+        if ($this->isProject)
+            {
+            $valid = $this->addProject(); 
+            if($valid){
+                $this->registry->heading = "Project Added!";
+                $this->registry->message = " has been added";
                 }
+                else {
+                    $this->registry->error = true;
+                    $this->registry->heading = "Error adding project";
+                    $this->registry->message = $this->newProject;
+                    }
+            }
+            $this->registry->View_Template->show('showMessage');
+        }
+
+    public function addProject()
+        {
+        $valid = false;
+
+        //assign to an array
+        $fields = array();
+
+        $fields['pName'] = $_POST['pName'];
+        $fields['pDescr'] = $_POST['pDescr'];
+        $fields['pStart'] = $_POST['pStart'];
+        $fields['pDead'] = $_POST['pDead'];
+        $fields['pln_hr'] = $_POST['pln_hr'];
+
+        $this->newProject = Project_Model::addProject($fields);
+        if (is_bool($this->newProject))
+            {
+            $valid = true;
+            } elseif (is_array($this->newProject) ||
+                is_string($this->newProject))
+            {
+            $valid = false;
+            }
+        return $valid;
+        }
+
+    public function addTask()
+        {
+        
+        }
+
     }

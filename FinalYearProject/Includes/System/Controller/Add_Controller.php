9ab2431bf5ea391dd61ba3717752108e50b1f481
james55555 <grahamj1@aston.ac.uk>
2014-03-28 12:40:42 +0000

    added is project chcekc

--- FinalYearProject/Includes/System/Controller/Add_Controller.php
+++ FinalYearProject/Includes/System/Controller/Add_Controller.php
@@ -14,9 +14,9 @@ class Add_Controller extends Main_Controller
     public function main()
         {
         //if is task or is project)
-        if ($_GET['page'] === 'Home')
-            {
-            $this->isProject = true;
+        
+            if($_GET['isProj'])
+                {
             $this->registry->View_Template->show('addProject');
             } else
             {
@@ -29,7 +29,7 @@ class Add_Controller extends Main_Controller
      * @param String $errDetails - then print out details
      */
 
-    public function showError($err, $errDetails)
+    public function showError()
         {
         if ($this->isProject)
             {
@@ -50,14 +50,14 @@ class Add_Controller extends Main_Controller
     public function addProject()
         {
         $valid = false;
-
+        $this->isProject = true;
         //assign to an array
         $fields = array();
 
         $fields['pName'] = $_POST['pName'];
         $fields['pDescr'] = $_POST['pDescr'];
         $fields['pStart'] = $_POST['pStart'];
-        $fields['pDead'] = $_POST['pDead'];
+        $fields['pDead'] = $_POST['pDeadline'];
         $fields['pln_hr'] = $_POST['pln_hr'];
 
         $this->newProject = Project_Model::addProject($fields);
@@ -74,7 +74,27 @@ class Add_Controller extends Main_Controller
 
     public function addTask()
         {
-        
+        $valid = false;
+        $this->isProject = true;
+        //assign to an array
+        $fields = array();
+
+        $fields['tName'] = $_POST['tName'];
+        $fields['tDescr'] = $_POST['tDescr'];
+        $fields['tStart'] = $_POST['tStart'];
+        $fields['tDead'] = $_POST['pDeadline'];
+        $fields['pln_hr'] = $_POST['pln_hr'];
+
+        $this->newProject = Task_Model::addTask($fields);
+        if (is_bool($this->newProject))
+            {
+            $valid = true;
+            } elseif (is_array($this->newProject) ||
+                is_string($this->newProject))
+            {
+            $valid = false;
+            }
+        return $valid;
         }
 
     }

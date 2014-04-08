439a6e9ceaf8cfcffa6c669f520ba7908ef67db0
james55555 <grahamj1@aston.ac.uk>
2014-04-08 12:29:57 +0100

    added function comments

--- FinalYearProject/Includes/common/Scripts/confirmAction.js
+++ FinalYearProject/Includes/common/Scripts/confirmAction.js
@@ -1,5 +1,9 @@
 /*
  * Function to prompt the user to confirm their action choice
+ * @param action (String) This is used to define what has been called
+ * @param item   (String) This is used to define the item being actioned
+ * 
+ * @return confirm() Return the popup box displaying the full String
  */
 function confirmAction(action, item){
     return confirm("Are you sure you want to " + action + " " + item + "?");

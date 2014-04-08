07ff1ef787ba155f835398f47d6bad6926a1321e
james55555 <grahamj1@aston.ac.uk>
2014-04-08 09:01:09 +0100

    formatted for compatibility with CSS

--- FinalYearProject/Includes/System/View/addProject.php
+++ FinalYearProject/Includes/System/View/addProject.php
@@ -8,9 +8,10 @@
 <html>
     <!DOCTYPE html>
     <head>
-        <script type="text/javascript" src="Includes/common/Scripts/Validation.js"></script>
-        <link rel="stylesheet" type="text/css" href="CSS/reset.css"/>
-        <link rel="stylesheet" type="text/css" href="CSS/main.css"/>
+        <link rel="stylesheet" type="text/css" href="Includes/CSS/reset.css"/>
+        <link rel="stylesheet" type="text/css" href="Includes/CSS/main.css"/>
+        <link rel="stylesheet" type="text/css" href="Includes/CSS/addProject.css"/>
+
         <title>Time Management System for Professionals - Add New Project page</title>
     </head>
     <body>
@@ -18,25 +19,27 @@
             <?php
             include("header.php");
             ?>
-            <div class="centralBox" id="content"> 
+            <div id="content" class="centralBox"> 
                 <h1>Add Project</h1>
 
                 <form id="addProject" action="?page=add&action=addProject&isProj=true" 
-                      method="post" onsubmit="return projectValidation()">
+                      method="post"> <!--onsubmit="return projectValidation()"-->
                     <div id="details">
-                        <label>Name: <input type="text" name="pName"/></label><br/>
-                        <label>Description:  <textarea 
-                                rows=4
-                                cols=50
+                        <h2>Details</h2>
+                        <label>Name: </label><input type="text" name="pName"/><br/>
+                        <label>Description:  </label><textarea 
                                 maxlength="200"
                                 name="pDescr"
-                                placeholder="Limit of 200 characters"></textarea></label>
+                                placeholder="Limit of 200 characters"></textarea>
                     </div> <!--End of details-->
-                    <div class="regInput" id="projDates">
+                    <div id="projDates">
                         <h2>Project Dates</h2>
-                        <label>Start Date: <input type="date" name="pStart"/></label>
-                        <label>Deadline: <input type="date" name="pDeadline"/></label>
-                        <label>Estimated Hours assigned: <input type="text" name="pln_hr"></label>
+                        <label title="When will the project start?">
+                            Start Date: </label><input type="date" name="pStart"/>
+                        <label title="When will the project end?">
+                            Deadline: </label><input type="date" name="pDeadline"/>
+                        <label title="How many hours will be assigned to the project?">
+                            Estimate: </label><input type="text" name="pln_hr"/>
                     </div> <!--End of projDates-->
 
                     <input type="submit" value="Submit" class="button" id="newProj"/>

82a1be26535fb9a56ea4cfbfcec6a9c617963bf0
james55555 <grahamj1@aston.ac.uk>
2014-04-08 12:28:36 +0100

    incorporated further extra parameter to pass action and item being actioned

--- FinalYearProject/Includes/common/Scripts/confirmAction.js
+++ FinalYearProject/Includes/common/Scripts/confirmAction.js
@@ -1,6 +1,6 @@
 /*
  * Function to prompt the user to confirm their action choice
  */
-function confirmAction(item){
-    return confirm("Are you sure you want to delete " + item + "?");
+function confirmAction(action, item){
+    return confirm("Are you sure you want to " + action + " " + item + "?");
 }

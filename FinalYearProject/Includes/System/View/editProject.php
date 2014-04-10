ef08de3eeaf0510bc71fdc1ed3aa798b50f46d54
james55555 <grahamj1@aston.ac.uk>
2014-04-10 01:34:09 +0100

    redesigned to fit edit not add

--- FinalYearProject/Includes/System/View/editProject.php
+++ FinalYearProject/Includes/System/View/editProject.php
@@ -1,46 +1,55 @@
 <?php
-
 #Copyright 2014 - Aston University
 #Final Year Project - CS3010
 #
-#Document: editProject
+#Document: addProject
 #@author: James
 ?>
 <html>
     <!DOCTYPE html>
     <head>
-        <script type="text/javascript" src="Includes/common/Scripts/Validation.js"></script>
-        <link rel="stylesheet" type="text/css" href="CSS/reset.css"/>
-        <link rel="stylesheet" type="text/css" href="CSS/main.css"/>
-        <title>Time Management System for Professionals - Add New Project page</title>
+        <script type="text/javascript" src="Includes/common/Scripts/confirmAction.js"></script>
+        
+        <link rel="stylesheet" type="text/css" href="Includes/CSS/reset.css"/>
+        <link rel="stylesheet" type="text/css" href="Includes/CSS/main.css"/>
+        <link rel="stylesheet" type="text/css" href="Includes/CSS/editProject.css"/>
+
+        <title>Time Management System for Professionals - Edit Project page</title>
     </head>
     <body>
+        <?php
+        include("header.php");
+        ?>
         <div id="container">
-            <?php
-            include("header.php");
-            ?>
-            <div class="centralBox" id="content"> 
-                <h1>Add Project</h1>
+            <div id="content" class="centralBox"> 
+                <h1>Edit Project</h1>
 
-                <form id="addProject" action="?page=add&action=addProject&isProj=true" 
-                      method="post" onsubmit="return projectValidation()">
+                <form id="editProject" action="?page=Edit&action=editProject" 
+                      method="post"> <!--onsubmit="return projectValidation()"-->
                     <div id="details">
-                        <label>Name: <input type="text" name="pName"/></label><br/>
-                        <label>Description:  <textarea 
-                                rows=4
-                                cols=50
-                                maxlength="200"
-                                name="pDescr"
-                                placeholder="Limit of 200 characters"></textarea></label>
+                        <h2>Details</h2>
+                        <label>Name: </label><input type="text" name="pName"/><br/>
+                        <label>Description:  </label><textarea 
+                            maxlength="200"
+                            name="pDescr"
+                            placeholder="Limit of 200 characters"></textarea>
                     </div> <!--End of details-->
-                    <div class="regInput" id="projDates">
+                    <div id="projDates">
                         <h2>Project Dates</h2>
-                        <label>Start Date: <input type="date" name="pStart"/></label>
-                        <label>Deadline: <input type="date" name="pDeadline"/></label>
-                        <label>Estimated Hours assigned: <input type="text" name="pln_hr"></label>
+                        <label>
+                            Start Date: </label><input type="date" name="pStart"/>
+                        <label>
+                            Deadline: </label><input type="date" name="pDeadline"/>
+                        <label>
+                            Estimate: </label><input type="text" name="pln_hr"/>
                     </div> <!--End of projDates-->
-
+                    <input type="hidden" value="<?php echo $_GET['proj_id']; ?>" name="proj_id"/>
+                    <input type="button" value="Back" class="button"
+                               onclick="history.go(-1);"/>
                     <input type="submit" value="Submit" class="button" id="newProj"/>
+                    <input type="reset" value="Reset" class="button" 
+                           onclick="return confirmAction('reset',
+                           'all values')"/>
                 </form>    
             </div><!--End of content-->
         </div><!--End of container-->

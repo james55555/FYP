0663a4d05ba91f0fd4d7c33513ab68bb8240b9fa
james55555 <grahamj1@aston.ac.uk>
2014-04-10 14:43:16 +0100

    added temporary message

--- FinalYearProject/Includes/System/Controller/Reports_Controller.php
+++ FinalYearProject/Includes/System/Controller/Reports_Controller.php
@@ -1,8 +1,22 @@
 <?php
 
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 
+class Reports_Controller extends Main_Controller
+    {
+
+    public function main()
+        {
+
+        $this->registry->error = true;
+        $this->registry->heading = "This page is under construction";
+        $this->registry->message = "This will be completed: 11/04/2014";
+
+        $this->registry->View_Template->show('showMessage');
+        }
+
+    }

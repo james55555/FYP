969eadd7dde8ed22cea1ab3ec2eb4ddf0b5f6ebd
james55555 <grahamj1@aston.ac.uk>
2014-04-14 01:02:35 +0100

    dependency ID now returns as an array

--- FinalYearProject/Includes/System/Model/TaskDependency_Model.php
+++ FinalYearProject/Includes/System/Model/TaskDependency_Model.php
@@ -30,25 +30,38 @@ class TaskDependency_Model extends Dependency_Model
         return $this->dp_id;
         }
 
-        //Get dependency id based on the task id provided
+    /*
+     * Function to get all dependency ID's associated with task in an array
+     * @param (String) $tsk_id          This is the task ID the ID's are associated with
+     * 
+     * @return (array) $dependencies    This is the array of task ID's that the provided $tsk_id is dependent on
+     */
+
     public static function getDpID($tsk_id)
         {
-        $row = Database_Queries::selectFrom("DEPENDENCY", "DEPENDENCY_ID", 
-                "TASK_DEPENDENCY", "TSK_ID", $tsk_id);
-        if(isset($row)){
-        $dpId = $row->DEPENDENCY_ID;
-        }
-        else {
-            $dpId = null;
+        $row = Database_Queries::selectFrom("DEPENDENCY_MODEL", "DEPENDENCY_ID", "TASK_DEPENDENCY", "TSK_ID", $tsk_id);
+        if (isset($row))
+            {
+            //Create array to store rows in
+            $dpIds = array();
+            //Assign list of Id's to the new array
+            foreach ($row as $id)
+                {
+                $dpIds = $id->DEPENDENCY_ID;
+                }
+            } else
+            {
+            //If nothing is found return null
+            $dpIds = null;
             }
-        return $dpId;
+        return $dpIds;
         }
 
     public static function delete($tsk_id)
         {
-        
-    $table = "TASK_DEPENDENCY";
-    $field = "DEPENDENCY_ID";
+
+        $table = "TASK_DEPENDENCY";
+        $field = "DEPENDENCY_ID";
         $success = Generic_Model::__delete($tsk_id, $table, $field);
         return $success;
         }

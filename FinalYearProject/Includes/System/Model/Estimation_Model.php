25d7c73b0db624f815b62c7703a048363b436ede
james55555 <grahamj1@aston.ac.uk>
2014-04-21 15:02:57 +0100

    added add method

--- FinalYearProject/Includes/System/Model/Estimation_Model.php
+++ FinalYearProject/Includes/System/Model/Estimation_Model.php
@@ -18,22 +18,19 @@ class Estimation_Model extends Generic_Model
             $act_end_dt;
     private
             $est_end_dt;
-    private 
+    private
             $est_id;
 
     public
             function __construct($est_id)
         {
-        try {
+        try
+            {
             $row = $this->get($est_id);
-            if(!isset($row)){
-                throw new Exception("Error in Estimation_Model<br/>"
-                    . "Row has been set to " . print_r($row));
+            if (!isset($row))
+                {
+                throw new Exception("NULL");
                 }
-            } catch (Exception $ex) {
-            return $ex->getMessage();
-            }
-            
             //If the no error has been thrown then continue creating object
             $this->est_id = $row->est_id;
             $this->act_hr = $row->act_hr;
@@ -41,6 +38,26 @@ class Estimation_Model extends Generic_Model
             $this->start_dt = $row->start_dt;
             $this->act_end_dt = $row->act_end_dt;
             $this->est_end_dt = $row->est_end_dt;
+            //If the value is set as String NULL then set to null value
+            foreach ($row as $key => $val)
+                {
+                if ($val === "NULL")
+                    {
+                    $this->set($key, null);
+                    }
+                }
+            } catch (Exception $e)
+            {
+            $var = $e->getMessage();
+            //Insert four values of "NULL" into $fields
+            $fields = array();
+            for ($i = 0; $i !== 4; $i++)
+                {
+                array_push($fields, $var);
+                }
+            //Add a new, empty staff object
+            $this->add($fields);
+            }
         }
 
     public function est_id()
@@ -73,12 +90,43 @@ class Estimation_Model extends Generic_Model
         return $this->est_end_dt;
         }
 
+    /*
+     * Inherit method to set a new variable
+     */
+
+    public function set($variable, $newValue)
+        {
+        try
+            {
+            $variable = strtolower($variable);
+
+            $this->$variable = $newValue;
+            if ($this->$variable !== $newValue)
+                {
+                throw new Exception("Error setting new value");
+                }
+            } catch (Exception $ex)
+            {
+            echo $ex->getMessage() . "<br/>" . $ex->getTraceAsString();
+            }
+        }
+
+    public function setEmptyNull($row)
+        {
+        foreach ($row as $key => $val)
+            {
+            if ($val === "NULL")
+                {
+                $this->set($key, null);
+                }
+            }
+        }
+
     public static function get($est_id)
         {
         $fields = array("est_id", "act_hr", "pln_hr", "start_dt",
             "act_end_dt", "est_end_dt");
-        $estimation = Database_Queries::selectFrom("ESTIMATION_MODEL", $fields, 
-                "ESTIMATION", "EST_ID", $est_id);
+        $estimation = Database_Queries::selectFrom("ESTIMATION_MODEL", $fields, "ESTIMATION", "EST_ID", $est_id);
         return $estimation;
         }
 
@@ -93,7 +141,7 @@ class Estimation_Model extends Generic_Model
         $db = new Database();
         $db->connect();
 
-    //Query returns all estimation models for all associated tasks whcih are associated with projects which are associated with users in the current session
+        //Query returns all estimation models for all associated tasks whcih are associated with projects which are associated with users in the current session
         $query = "SELECT EST_ID, ACT_HR, PLN_HR, START_DT,"
                 . " ACT_END_DT, EST_END_DT, EST.TSK_ID"
                 . " FROM ESTIMATION EST"
@@ -139,6 +187,39 @@ class Estimation_Model extends Generic_Model
         $success = parent::__delete($est_id, $table, $field);
 
         return $success;
+        }
+
+    public static function add($fields)
+        {
+        $db = new Database();
+        $db->connect();
+        mysql_query("START TRANSACTION;");
+        //Set insert into ESTIMATION
+        $estimation_insert = "INSERT INTO ESTIMATION ("
+                . "EST_ID, "
+                . "ACT_HR, "
+                . "PLN_HR, "
+                . "START_DT, "
+                . "ACT_END_DT, "
+                . "EST_END_DT) "
+                . "VALUES( "
+                . "NULL, "
+                . "'" . $fields[0] . "'', "
+                . "'" . $fields[1] . "', "
+                . "'" . $fields[2] . "', "
+                . "'" . $fields[3] . "', "
+                . "'" . $fields[4] . "');";
+        //Run query and get estimation id.
+        $estimation_result = mysql_query($estimation_insert);
+        if ($db->endStatement($estimation_result))
+            {
+            $estimation = new Estimation_Model($db->getInsertId());
+            } else
+            {
+            $estimation = "Error inserting into estimation";
+            }
+        $db->close();
+        return $estimation;
         }
 
     }

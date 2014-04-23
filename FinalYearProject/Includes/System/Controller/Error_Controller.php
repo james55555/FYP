5fa5342d5474a8c19dd79e6030bf932196634fe2
james55555 <grahamj1@aston.ac.uk>
2014-04-23 13:51:17 +0100

    pointed class name to error controller

--- FinalYearProject/Includes/System/Controller/Error_Controller.php
+++ FinalYearProject/Includes/System/Controller/Error_Controller.php
@@ -2,13 +2,13 @@
 /*
  * Controller class to run the command to show the error page.
  */
-class Error404_Controller extends Main_Controller
+class Error_Controller extends Main_Controller
     {
-        public function main()  {
+        public function main()  { 
             //run the show function in View_Template for page 404.php
             $this->registry->error = true;
-            $this->registry->title = "Error 404";
-            $this->registry->message = "There has been a redirecting issue...";
+            $this->registry->heading = "Error 404";
+            $this->registry->message = "There has been a redirect issue.";
             $this->registry->View_Template->show('showMessage');
             }
     }

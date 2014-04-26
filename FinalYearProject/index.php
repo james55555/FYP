8619ed3c8ecce46383768b525f19075bda278b1f
james55555 <grahamj1@aston.ac.uk>
2014-04-26 22:34:01 +0100

    formatted comments

--- FinalYearProject/index.php
+++ FinalYearProject/index.php
@@ -3,20 +3,26 @@
  *  Namespace       : Vendor
  *  Document        : index.php
  *  Author          : James Graham
- *  Description     : initiate master class @autload
+ *  Description     : initiate master class @autoload
  * 
  */
+
+ //Load up the configuration (inc site_path and error reporting)
 require "configuration.php";
 
-//Start session
+//Start the session
 session_start();
-//load router into registry
+
+//Load the router
 $registry->router = new router($registry);
+
 //Set the path of the controller
 $registry->router->setPath(__CONTROLLER_PATH);
-//Load the template based on action object
+
+//Load the template
 $registry->View_Template = new View_Template($registry); 
-//run loader function in router to load controller
+
+//Load the controller
 $registry->router->loader();
 
 ?>

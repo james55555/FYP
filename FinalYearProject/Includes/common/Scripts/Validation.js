3e7f09f9a15c5e923497ecb34f6493d91f25298b
james55555 <grahamj1@aston.ac.uk>
2014-04-29 01:35:52 +0100

    Updated logic to handle with try and catch to reduce code-reuse

--- FinalYearProject/Includes/common/Scripts/Validation.js
+++ FinalYearProject/Includes/common/Scripts/Validation.js
@@ -9,55 +9,62 @@ jQuery(function($) {
     var form = document.forms[0];
 
     $("#" + form.id + " input[name='submit']").click(function(e) {
+
         var errors = [];
-        var empty = [];
 
         //Check email contains the right characters
         var em_chars = /([\w\-]+\@[\w\-]+\.[\w\-]+)/;
-        //Get all input elements that are valid
-        var fieldStack = document.querySelectorAll('.valid');
-        //Check for empty inputs
-        for (i = 0; i < fieldStack.length; i++) {
-            //Similar to associative key => value assignment
-            //Key - Field name
-            var field = fieldStack[i];
-            //Value - Value name
-            var val = form[field.name].value.trim();
-            //Allow empty optional fields
-            if (!$(field).data('optional')) {
-                if (!val) {
-                    $("#" + field.name).removeClass("valid").addClass('error');
-                    $("span#sp" + field.id).html("*");
-                    empty.push(field.name);
-                }
-            }
-            //If the field type is an email then perform validation
-            else if (field.name === "email" && val !== "") {
-                //Validate email type
-                if (!em_chars.test(val)) {
+
+        //Get all input elements that are to be validated
+        var fieldStack = document.querySelectorAll("#" + form.id + " input[data-validate='true']");
+
+        try {
+            //Check for empty inputs
+            for (i = 0; i < fieldStack.length; i++) {
+                try {
+                    //Similar to associative key => value assignment
+                    //Key - Field name
+                    var field = fieldStack[i];
+                    //Value - Value name
+                    var val = form[field.name].value.trim();
+                    //Allow empty optional fields
+                    if (!$(field).data('optional')) {
+                        if (!val) {
+                            throw("Fields Required!");
+                        }
+                    }
+                    //If the field type is an email then perform validation
+                    else if (field.name === "email" && val !== "") {
+                        //Validate email type
+                        if (!em_chars.test(val)) {
+                            throw("Invalid email");
+                        }
+                    }
+                } catch (ex1) {
+                    errors.push(field.name);
+                    /*
+                     * Error messages will show as expected if this criteria is 
+                     * met in the HTML document:
+                     *      .error class exists in CSS
+                     *      A span class with and id of 'sp' followed by the input id
+                     *          is positioned to the right of each input
+                     *      Two divs or spans exist for form instructions and error message
+                     *          Their id's must be #msg and #errMessage respectively
+                     */
                     $("#" + field.name).addClass('error');
                     $("span#sp" + field.id).html("*");
-                    errors.push(field.name);
+                    $("#msg").hide();
+                    $("#errMessage").html(ex1);
                 }
+            }//End of for loop
+            if (errors.length !== 0) {
+                throw (false);
             }
-        }//End of for loop
-        //Message to explain error
-        var msg;
-        if (empty.length !== 0) {
-            msg = "Fields Required!";
+        } catch (ex2) {
+            //Only caught when the loop has iterated through all nodeList variables
+            //and errors have been caught
+            return ex2;
         }
-        else if (errors.length !== 0)
-        {
-            //Capitalize first letter of error field and concatenate
-            msg = errors[0].charAt(0).toUpperCase()
-                    + errors[0].substring(1)
-                    + " isn't valid!";
-        }
-        else {
-            return true;
-        }
-        $("#msg").hide();
-        $("#errMessage").html(msg);
-        return false;
+        return true;
     });
 });
